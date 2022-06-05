4e1eb1e57ac6e1ec1ef1dbba5948fde9a76a4efb
Marc-André Émond <marc-andre.emond@tlmgo.com>
2022-06-05 18:52:05 -0400

    Fix typo error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoString = process.env.DATABASE_URL;
 const app = express();
 
 mongoose.connect(mongoString);
-const database = mongoose.connection();
+const database = mongoose.connection;
 
 database.on("error", (error) => {
   console.log(error);
